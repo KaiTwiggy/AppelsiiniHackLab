e0bffb33dca6e53724fa0ac07472d333549e0bc8
mikri <mikko.hyytiainen@gmail.com>
2015-03-07 18:41:53 +0200

    post changes

--- Website/Hacklab2015/Scripts/app.js
+++ Website/Hacklab2015/Scripts/app.js
@@ -11,12 +11,18 @@
 			apiEndPoint = '/Api/Relay'
 			submittingClass = 'btn-submitting',	
 			successClass = 'btn-success3d',	
-			errorClass = 'btn-error3d';
+			errorClass = 'btn-error3d',
+			_id = $this.data('id');
 		
 		$this.addClass(submittingClass);
 
+		$.post(apiEndPoint, {'id':_id}, function(data, textStatus, xhr) {
 
-		$.post(apiEndPoint, {}, function(data, textStatus, xhr) {
+		})
+		.error(function(){
+			$this.addClass(errorClass).removeClass(submittingClass);
+		})
+		.done(function(){
 			$this.addClass(successClass).removeClass(submittingClass);
 		});
 
