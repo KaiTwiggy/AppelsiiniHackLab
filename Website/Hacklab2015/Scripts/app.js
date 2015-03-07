97fb2c7a0937ee72e540b85e67819551c4a8e9a6
mikri <mikko.hyytiainen@gmail.com>
2015-03-07 18:28:14 +0200

    post

--- Website/Hacklab2015/Scripts/app.js
+++ Website/Hacklab2015/Scripts/app.js
@@ -8,17 +8,18 @@
 		event.preventDefault();
 
 		var $this = $(this),
+			apiEndPoint = '/Api/Relay'
 			submittingClass = 'btn-submitting',	
 			successClass = 'btn-success3d',	
 			errorClass = 'btn-error3d';
 		
 		$this.addClass(submittingClass);
 
-		setTimeout(function(){
+
+		$.post(apiEndPoint, {}, function(data, textStatus, xhr) {
 			$this.addClass(successClass).removeClass(submittingClass);
-		},
-			2000
-		);
+		});
+
 
 
 	});
